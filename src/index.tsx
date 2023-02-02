6b870944c8ce30f1b5985df5bf4ab74d122e4dd5
jjangjun <95jjangjun@gmail.com>
2023-02-02 23:03:27 +0900

    chore: eslint, prettier 설정

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import App from "App";
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const rootNode = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootNode);
 root.render(<App />);
